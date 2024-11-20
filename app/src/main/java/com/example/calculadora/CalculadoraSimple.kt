c3b52bbe12df3ead6c8607e4f06638d99f9843ea
Javi <fmontesestrabon@safarayes.es>
2024-11-20 12:02:52 +0100

    Creado xml calculadora simple

---
+++ app/src/main/java/com/example/calculadora/CalculadoraSimple.kt
@@ -0,0 +1,12 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculadoraSimple : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calculadora_simple)
+
+    }
+}
